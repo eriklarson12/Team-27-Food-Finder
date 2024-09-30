118015a268810c3a83a81cff8802f8b37059f073
eriklarson12 <erik.larson12@outlook.com>
2024-09-30 16:49:04 -0400

    final push

--- restaurant_finder/accounts/views.py
+++ restaurant_finder/accounts/views.py
@@ -1,6 +1,7 @@
 # accounts/views.py
 
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -12,10 +13,11 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}. You can now log in.')
-            return redirect('login')
+            messages.success(request, f'Account created for {username}. You are now logged in.')
+            login(request, user)
+            return redirect('restaurants:home')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
@@ -26,8 +28,15 @@ def profile(request):
     context = {
         'favorites': favorites,
     }
-
     return render(request, 'accounts/profile.html', context)
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        messages.success(request, 'You have been successfully logged out.')
+        return redirect('restaurants:home')
+    else:
+        return redirect('restaurants:home')
 
 @login_required
 def toggle_favorite(request, restaurant_id):
